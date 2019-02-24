b26369b54ad22ca520e2b2d6e47a9d1fd3e67737
javi <javi@192.168.1.10>
2019-02-24 11:12:31 +0100

    Components as tag

--- src/main/java/io/github/thymeleaf/ui/dialect/tags/NavigationHeaderAttributeTagProcessor.java
+++ src/main/java/io/github/thymeleaf/ui/dialect/tags/NavigationHeaderAttributeTagProcessor.java
@@ -1,11 +1,13 @@
 package io.github.thymeleaf.ui.dialect.tags;
 
+import org.thymeleaf.context.IEngineContext;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.engine.AttributeName;
 import org.thymeleaf.model.IProcessableElementTag;
 import org.thymeleaf.processor.element.IElementTagStructureHandler;
 
 import io.github.thymeleaf.ui.components.NavigationHeader;
+import io.github.thymeleaf.ui.components.Sidebar;
 
 public class NavigationHeaderAttributeTagProcessor extends AbstractTagProcessor {
     
@@ -18,10 +20,28 @@ public class NavigationHeaderAttributeTagProcessor extends AbstractTagProcessor 
     @Override
     protected void doProcess(ITemplateContext context, IProcessableElementTag tag, AttributeName attributeName,
             String attributeValue, IElementTagStructureHandler structureHandler) {
+        NavigationHeader nav = resolve(attributeValue, context);
+        render(nav, context, tag, structureHandler, attributeName);
+    }
+    
+    private NavigationHeader resolve(String attributeValue, ITemplateContext context) {
+        if(attributeValue==null) {
+            Object variable = context.getVariable("navigationHeader");
+            return variable instanceof Sidebar ? (NavigationHeader) variable : new NavigationHeader();
+        }
+        
         Object target = evaluate(attributeValue, context);
         boolean isRenderable = target instanceof NavigationHeader;
-        NavigationHeader nav = isRenderable ? (NavigationHeader) target : new NavigationHeader();
-        render(nav, context, tag, structureHandler, attributeName);
+        if(isRenderable) {
+            return (NavigationHeader) target;
+        }
+        
+        NavigationHeader navHeader = new NavigationHeader();
+        if(target instanceof String) {
+            ((IEngineContext)context).setVariable((String)target, navHeader);
+        }
+        
+        return navHeader;
     }
     
 }
--- src/main/java/io/github/thymeleaf/ui/dialect/tags/SidebarAttributeTagProcessor.java
+++ src/main/java/io/github/thymeleaf/ui/dialect/tags/SidebarAttributeTagProcessor.java
@@ -1,5 +1,6 @@
 package io.github.thymeleaf.ui.dialect.tags;
 
+import org.thymeleaf.context.IEngineContext;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.engine.AttributeName;
 import org.thymeleaf.model.IProcessableElementTag;
@@ -18,10 +19,28 @@ public class SidebarAttributeTagProcessor extends AbstractTagProcessor {
     @Override
     protected void doProcess(ITemplateContext context, IProcessableElementTag tag, AttributeName attributeName,
             String attributeValue, IElementTagStructureHandler structureHandler) {
+        Sidebar sidebar = resolve(attributeValue, context);
+        render(sidebar, context, tag, structureHandler, attributeName);
+    }
+    
+    private Sidebar resolve(String attributeValue, ITemplateContext context) {
+        if(attributeValue==null) {
+            Object variable = context.getVariable(ATTRIBUTE_NAME);
+            return variable instanceof Sidebar ? (Sidebar) variable : new Sidebar();
+        }
+        
         Object target = evaluate(attributeValue, context);
         boolean isRenderable = target instanceof Sidebar;
-        Sidebar sidebar = isRenderable ? (Sidebar) target : new Sidebar();
-        render(sidebar, context, tag, structureHandler, attributeName);
+        if(isRenderable) {
+            return (Sidebar) target;
+        }
+        
+        Sidebar sidebar = new Sidebar();
+        if(target instanceof String) {
+            ((IEngineContext)context).setVariable((String)target, sidebar);
+        }
+        
+        return sidebar;
     }
 
 }
